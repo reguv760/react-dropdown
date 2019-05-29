278d4ac77870e5f1007e97fd0b1db730e89aea23
reguv760 <reguv760@gmail.com>
2019-05-29 08:45:10 -0700

    Updates

---
+++ precache-manifest.bb308aa6b6988d5ec298eb4b5a0781c1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81dc852bba569dfffe5842b88da0ebd9",
+    "url": "/react-sidedrawer/index.html"
+  },
+  {
+    "revision": "1d604d61a814024a7897",
+    "url": "/react-sidedrawer/static/css/main.710cecc4.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/react-sidedrawer/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "1d604d61a814024a7897",
+    "url": "/react-sidedrawer/static/js/main.17d80f2c.chunk.js"
+  },
+  {
+    "revision": "743929215f88ed8f0145",
+    "url": "/react-sidedrawer/static/js/runtime~main.16642937.js"
+  }
+]);
